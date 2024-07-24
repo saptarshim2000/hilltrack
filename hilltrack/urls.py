7a699aa5e3f006258512d93849df345c0c01022b
saptarshim2000 <saptarshim2000@gmail.com>
2024-07-24 13:24:26 +0100

    Fix view import error and serve React frontend

--- hilltrack/urls.py
+++ hilltrack/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from formapp.views import home  # Import the home view
+from formapp.views import index  # Import the correct view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('formapp.urls')),
-    path('', home, name='home'),  # Add the home view for the root URL
+    path('', index, name='index'),  # Use the correct view
 ]
